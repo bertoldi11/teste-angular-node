ce4d22c4cf0503cc01807d6a2763d650fb5f440e
Vinicius Bertoldi <vinicius@print4me.com.br>
2015-02-20 14:24:02 -0200

    Parte 3 do tutorial

--- source/api.js
+++ source/api.js
@@ -5,12 +5,18 @@ var objRetorno = [
     {text:'build an angular app', done:false}
 ];
 
+var fncCarrinho = function (req, res){
+
+};
+
 app.get('/', function(req, res) {
     console.log('Entrou no GET / ');
     res.charset = 'utf-8';
     res.type('application/json');
     res.json(objRetorno);
 });
+
+app.get('/carrinho',fncCarrinho);
 
 app.listen(process.env.PORT || 3000);
 console.log('Server running at http://127.0.0.1:3000');
